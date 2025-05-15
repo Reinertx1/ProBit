3195b377ec3c389bc76701276f9a7bbebd9f1cd6
Reiner <116982053+Reinertx1@users.noreply.github.com>
2025-05-15 03:55:37 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  token:"MTA4MTU3ODUxMTE3NDc1MDI3OA.GdzNM7.mcUwIxEStSU8v19Zcqr_EEnIKtaVo5dsy1V-hI",
+  token:"MTA4MTU3ODUxMTE3NDc1MDI3OA.Gq4rGu.rZmZLgGVuU41i0uSYFvgDOihyaSxo2eiaINEpA",
   globalBot: true, // false => if bot is private bot - true => if bot isn't private bot
   guildID: "1034469502311747705",
 }
